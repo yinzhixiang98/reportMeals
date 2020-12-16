eee8800854f4a41d6bfb793ce85d0058afb5eff4
yinzhixiang <yinzhixiang@ygsoft.com>
2020-12-16 14:57:49 +0800

    修改截屏python文件

--- ygjy_reportMeals/function/screenshot.py
+++ ygjy_reportMeals/function/screenshot.py
@@ -4,11 +4,11 @@ from selenium import webdriver     #从selenium库导入webdirver
 
 def screenshot(save_path):
     global driver
-    chrome_path = "E:/chormedriver/74.0.3729.6/chromedriver.exe"
+    chrome_path = "D:/YG Soft/YGAUTOAPPIUM/chromedriver.exe"
     os.environ['webdriver.chrome.driver'] = chrome_path
     driver = webdriver.Chrome(chrome_path)
     sleep(1)
-    driver.get("file:///C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/workspace/ygjy_parkpass/default/reports/SummaryReport.html")
+    driver.get("file:///C:/windows/system32/config/systemprofile/AppData/Local/Jenkins/.jenkins\workspace/reportMeals/ygjy_reportMeals/reports/SummaryReport.html")
     driver.maximize_window()
     driver.implicitly_wait(10)
     driver.save_screenshot(save_path)
